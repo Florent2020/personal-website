5c903f07052f54323984fa5b668ea6be90216fa3
Florent <florent.hajdari@hotmail.com>
2023-02-21 00:37:38 +0100

    First commit

---
+++ src/components/dataProjects.js
@@ -0,0 +1,61 @@
+const projects = [
+  {
+    id: 1,
+    image:
+      "https://florent-coding.com/wp-content/uploads/2023/02/solheimfeltet.png",
+    project: "Solheimfeltet",
+    url: "https://www.solheimfeltet.no/",
+  },
+  {
+    id: 2,
+    image: "https://florent-coding.com/wp-content/uploads/2023/02/vigo-iks.png",
+    project: "Vigo IKS",
+    url: "https://www.vigoiks.no/",
+  },
+  {
+    id: 3,
+    image: "https://florent-coding.com/wp-content/uploads/2023/02/spaceX.png",
+    project: "Galaxy SpaceX",
+    url: "https://florent-coding.com/SpaceX_Project/",
+  },
+  {
+    id: 4,
+    image:
+      "https://florent-coding.com/wp-content/uploads/2023/02/galaxy_science_museum.png",
+    project: "Galaxy Science Museum",
+    url: "https://florent-coding.com/Galaxy_Science_Museum/",
+  },
+  {
+    id: 5,
+    image: "https://florent-coding.com/wp-content/uploads/2023/02/aarsleff.png",
+    project: "Aarsleff",
+    url: "https://www.aarsleff.weratest4.no/",
+  },
+  {
+    id: 6,
+    image: "https://florent-coding.com/wp-content/uploads/2023/02/nucleus.png",
+    project: "Nucleus",
+    url: "https://florent-coding.com/Nucleus/",
+  },
+  {
+    id: 7,
+    image: "https://florent-coding.com/wp-content/uploads/2023/02/letha.png",
+    project: "Letha Shoemaker",
+    url: "https://florent-coding.com/Letha_Shoemaker/",
+  },
+  {
+    id: 8,
+    image: "https://florent-coding.com/wp-content/uploads/2023/02/YAY.png",
+    project: "The YAY Company",
+    url: "https://florent-coding.com/The_YAY_Company/",
+  },
+  {
+    id: 9,
+    image:
+      "https://florent-coding.com/wp-content/uploads/2023/02/aqua-pharm.png",
+    project: "Aqua Pharm",
+    url: "https://aquapharm-ks.com/",
+  },
+];
+
+export default projects;
